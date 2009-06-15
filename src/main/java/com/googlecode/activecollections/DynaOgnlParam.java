2cfe4e4808c4c45c3d0f0ca861a8bc1af7013f00
opsb <oliver@opsb.co.uk>
2009-06-16 00:36:00 +0100

    Added DynaParam Added DynaOgnlParam

---
+++ src/main/java/com/googlecode/activecollections/DynaOgnlParam.java
@@ -0,0 +1,29 @@
+package com.googlecode.activecollections;
+
+import ognl.Ognl;
+import ognl.OgnlException;
+
+public class DynaOgnlParam implements DynaParam {
+
+	private Object root;
+	
+	private String ognl;
+	
+	public DynaOgnlParam(Object root, String ognl) {
+		this.root = root;
+		this.ognl = ognl;
+	}
+	
+	public Object getValue() {
+		try {
+			return Ognl.getValue(ognl, root);
+		} catch (OgnlException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public static DynaOgnlParam ognlParam(Object bean, String ognl) {
+		return new DynaOgnlParam(bean, ognl);
+	}
+
+}
---
+++ src/main/java/com/googlecode/activecollections/DynaParam.java
@@ -0,0 +1,7 @@
+package com.googlecode.activecollections;
+
+public interface DynaParam {
+
+	public Object getValue();
+	
+}
